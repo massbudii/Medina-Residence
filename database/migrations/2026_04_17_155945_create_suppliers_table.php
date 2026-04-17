0c819984f927e166bf10e225ffcd8a1c80bec5e1
masbudi <budisantoso.dev08@gmail.com>
2026-04-17 23:00:28 +0700

    ya

--- database/migrations/2026_04_17_155945_create_suppliers_table.php
+++ database/migrations/2026_04_17_155945_create_suppliers_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('table_suppliers', function (Blueprint $table) {
+        Schema::create('suppliers', function (Blueprint $table) {
             $table->id();
             $table->string('nama_supplier', 64)->unique();
             $table->text('alamat_supplier', 100);
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('table_suppliers');
+        Schema::dropIfExists('suppliers');
     }
 };
