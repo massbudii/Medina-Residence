839bf7d11bf8102f2865944817494e4fa161f50a
masbudi <budisantoso.dev08@gmail.com>
2026-06-11 15:49:15 +0700

    notif fungsi

--- database/migrations/2026_06_07_000001_create_app_notifications_table.php
+++ database/migrations/2026_06_07_000001_create_app_notifications_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('url');
             $table->timestamp('read_at')->nullable();
             $table->timestamps();
+            
         });
     }
 
