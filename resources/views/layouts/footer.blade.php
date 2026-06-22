28c9ceee7b43c7797a7ec11423a84f7ce2dbfa42
masbudi <budisantoso.dev08@gmail.com>
2026-06-22 13:09:46 +0700

    footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,8 +4,8 @@
              &copy;
              <script>
                  document.write(new Date().getFullYear())
-             </script> - Made with <span class="mdi mdi-heart text-danger"></span> by <a href="#!"
-                 class="text-reset fw-semibold">Zoyothemes</a>
+             </script> - Copyright </span>  <a href="#!"
+                 class="text-reset fw-semibold">PT. Medina Aqsa Arthatama</a>
          </div>
      </div>
  </div>
